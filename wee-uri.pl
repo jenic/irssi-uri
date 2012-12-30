5b7098d34bb0629da7e50e47c188d019582cc9b2
Jenic Rycr <jenic@wubwub.me>
2012-12-30 14:10:41 -0700

    Now using hook_process to avoid hangups. Needs better security testing still

--- wee-uri.pl
+++ wee-uri.pl
@@ -10,8 +10,7 @@
 # You will need the following packages:
 # LWP::UserAgent (You can install this using cpan -i LWP::UserAgent)
 # Crypt::SSLeay  (You can install this using cpan -i Crypt::SSLeay)
-# 
-# Or if you're using Debian GNU/Linux:
+# Or if you're using Debian:
 # apt-get update;apt-get install libwww-perl libcrypt-ssleay-perl
 #
 # Script heavily modified by Jenic Rycr, list of changes:
@@ -26,13 +25,12 @@
 # Port from irssi to WeeChat
 # Complete code rewrite relicensed as GPL3
 # Caching Support for frequent urls and to guard against abuse
+# Weechat's hook_process to prevent I/O blocking
 #
 #####################################################################
 
 use strict;
-use LWP::UserAgent;
-use Carp qw(croak);
-use HTML::Entities;
+use HTML::Entities qw(decode_entities);
 
 my $self = 'uri';
 my $uribuf;
@@ -63,7 +61,7 @@ my %opt =	( 'debug'		=> 0
 
 weechat::register	( $self
 			, 'Jenic Rycr <jenic\@wubwub.me>'
-			, '0.9'
+			, '1.0'
 			, 'GPL3'
 			, 'URI Title Fetching'
 			, ''
@@ -72,8 +70,15 @@ weechat::register	( $self
 
 my $version = sprintf("%s", weechat::info_get('version',''));
 
+# Helper Subroutines
+sub debug {
+	return unless $opt{debug};
+	my $msg = shift;
+	weechat::print(weechat::current_buffer(), "[uri::debug]\t$msg");
+	return 1;
+}
 ## URL Blacklist
-#  Evaluated as regular expressions
+###  Evaluated as regular expressions
 sub blup {
 	return weechat::WEECHAT_RC_OK unless (-e $opt{blfile});
 	open FH, $opt{blfile} or return weechat::WEECHAT_RC_OK;
@@ -82,14 +87,6 @@ sub blup {
 	close FH;
 	@blacklist = @bl;
 	return weechat::WEECHAT_RC_OK;
-}
-
-# Helper Subroutines
-sub debug {
-	return unless $opt{debug};
-	my $msg = shift;
-	weechat::print(weechat::current_buffer(), "[uri::debug]\t$msg");
-	return 1;
 }
 sub chklist {
 	my $link = shift;
@@ -131,48 +128,66 @@ sub getNick {
 	return $nick;
 }
 
-# LWP Handler Subroutines
-sub headcheck {
-	my ($response, $ua, $h) = @_;
-	my $v = $response->header('Content-Type');
-	croak "complete" unless ($v && $v =~ /text\/html/);
-	return 0;
-}
-sub titlecheck {
-	my ($r, $ua, $h, $data) = @_;
-	&debug(join('::', @_));
-	if (!$r->is_redirect && $data =~ /<title>(?:.*?)<\/title>/is) {
-		debug("title found in chunk, croaking");
-		croak "complete";
-	}
+# Where the magic happens
+sub uri_get {
+	my ($uri, $buf) = @_;
+	my $c = 'perl -MLWP::UserAgent -MCarp -e\'' .
+	'sub hc{my($r,$u,$h)=@_;my $v=$r->header("Content-Type");croak "complete" if($v && $v !~ /text\/html/);return 0;}' .
+	'sub tc{my($r,$u,$h,$d)=@_;if(!$r->is_redirect && $d=~/<title>.*<\/title>/is){croak "complete";}return 1;}' .
+	'my $d=q(http://google.com);system(q(touch ~/test);my $u=LWP::UserAgent->new(env_proxy=>1,keep_alive=>1,timeout=>8);$u->max_size(1024);' .
+	'$u->add_handler(response_header=>*hc{CODE});$u->add_handler(response_data=>*tc{CODE});' .
+	'my $r=$u->get($d);print(($r->is_success)?$r->content:0);';
+	&debug("Hooking process for $uri");
+	weechat::hook_process($c, 10000, 'uri_process_cb', "@_");
 	return 1;
 }
-
-sub uri_get {
-	my ($data) = @_;
-	my $ua = LWP::UserAgent->new(env_proxy=>1, keep_alive=>1, timeout=>5);
-	$ua->agent("WeeChat/$version " . $ua->agent());
-	$ua->max_size(1024); # max 1MB download
-
-	# add header handler to stop if not html
-	$ua->add_handler(response_header => \&headcheck);
-
-	# add data handler to stop after we dl <title>
-	$ua->add_handler(response_data => \&titlecheck);
-
-	my $res = $ua->get($data);
-	return ($res->is_success) ? $res->content : 0;
-}
-
 # Callback Subroutines
+sub uri_process_cb {
+	my ($data, $cmd, $rc, $stdout, $stderr) = @_;
+	&debug(join('||',@_));
+	my $title;
+	my ($uri, $buffer) = split ' ', $data;
+	my $out = ($opt{mode}) ? $uribuf : $buffer;
+	if($stdout =~ /<title>(.*?)<\/title>/is) {
+		if(!$1) {
+			&debug("Pattern matched but title empty");
+			return weechat::WEECHAT_RC_OK;
+		}
+		$title = $1;
+	} else {
+		&debug("Gave up on matching <title>");
+		return weechat::WEECHAT_RC_OK;
+	}
+
+	# multiple small calls to engine more efficient than expressed in regex
+	$title =~ s/[\r\n]//g;
+	$title =~ s/\s+/ /g;
+	$title = decode_entities($title);
+
+	weechat::print($out, "[uri]\t$uri ($title)");
+	weechat::print($buffer, "[uri]\t$title")
+		if ($opt{mode} == 2);
+	# Add this to cache and do some cache pruning
+	# This pruning is a fall back incase proper prune fails for w/e reason.
+	# Don't want memory filling up with url's!
+	if (scalar keys %cache > ($opt{cache}*2)) {
+		&debug("Cache Prune Fallback! Something weird happened!");
+		%cache = ();
+	}
+	$cache{$uri}->{u} = $title;
+	$cache{$uri}->{t} = time;
+
+	return weechat::WEECHAT_RC_OK;
+}
 sub uri_cb {
 	my ($data, $buffer, $date, $tags, $disp, $hl, $prefix, $msg) = @_;
-	# Set Printing Mode
 	my $out = ($opt{mode}) ? $uribuf : $buffer;
-	&debug(join('::', @_));
 	my @url = &uri_parse($msg);
+
+	&debug(join('::', @_));
 	# there is no need to go beyond this point otherwise
 	return weechat::WEECHAT_RC_OK unless (@url > 0);
+	
 	unless($opt{xown}) {
 		my $nick = &getNick($buffer);
 		&debug("My nick is $nick");
@@ -189,34 +204,11 @@ sub uri_cb {
 			$cache{$uri}->{t} = $date;
 			next;
 		}
-		# No cache entry, get the uri
-		my $retval = uri_get($uri);
-		&debug("Raw retval = $retval");
-		if($retval =~ /<title>(.*?)<\/title>/is) {
-		$retval = $1;
-		} else {
-			&debug("Gave up on matching <title>");
-			next;
-		}
-		# multiple small calls to engine more efficient than expressed in regex
-		$retval =~ s/[\r\n]//g;
-		$retval =~ s/\s+/ /g;
-		$retval = decode_entities($retval);
 
-		weechat::print($out, "[uri]\t$uri ($retval)");
-		weechat::print($buffer, "[uri]\t$retval")
-			if ($opt{mode} == 2);
-		# Add this to cache and do some cache pruning
-		# This pruning is a fall back incase proper prune fails for w/e reason.
-		# Don't want memory filling up with url's!
-		if (scalar keys %cache > ($opt{cache}*2)) {
-			&debug("Cache Prune Fallback! Something weird happened!");
-			%cache = ();
-		}
-		$cache{$uri} = ();
-		$cache{$uri}->{u} = $retval;
-		$cache{$uri}->{t} = $date;
+		# No cache entry, get the uri
+		uri_get($uri, $buffer);
 	}
+	
 	# Cache Pruning
 	if(scalar keys %cache > $opt{cache}) {
 		my @ordered =	map { $_->[0] } # Undecorate
@@ -235,8 +227,10 @@ sub uri_cb {
 			delete $cache{$ordered[0]};
 		}
 	}
+
 	return weechat::WEECHAT_RC_OK;
 }
+
 sub toggle_opt {
 	my ($pointer, $option, $value) = @_;
 	my $o = (split /\./, $option)[-1];
